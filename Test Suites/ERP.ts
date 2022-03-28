082e219dde99009e6ba4d2b9842642fa2a42a852
chaitalidube <chaitali.dube@aurumproptech.in>
2022-03-28 17:37:10 +0530

    Suites

---
+++ Test Suites/ERP.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ERP</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d45ac4c8-f60b-4c6b-b68d-a31c94f65b18</testSuiteGuid>
+   <testCaseLink>
+      <guid>6706c312-06b0-45f9-9dc5-9a5802d06b61</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ERP/Test Case 1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8675f8ba-a82c-4a86-a5cd-991d0a5aeb97</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ERP/Test Case 2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
